967581569fe526a55d4768bce2814f35fd8da516
Mink_2551 <thefirestong@gmail.com>
2024-02-10 23:16:46 +0700

    Mini responsive fix 1

--- src/Sections/Header.jsx
+++ src/Sections/Header.jsx
@@ -4,12 +4,12 @@ import { data } from "../Content/About"
 const Header = () => {
     return(
         <div className="grid md:grid-cols-[60%_40%] m-5">
-             <div className="lg:mx-10 m-2 mr-16 mt-10 ">
+             <div className="lg:mx-10 m-2 mr-5 mt-10 ">
                 <h1 className="font-bold text-4xl flex"> { data.MainTitle } </h1>
                 <p className="flex font-medium text-xs ml-1 text-gray-500"> { data.SeccondaryTitle } </p>
                 <p className="flex font-medium text-md ml-1 text-gray-700 mt-5"> { data.Passages } </p>
              </div>
-             <div className="md:m-7 m-20 my-5">
+             <div className="md:m-7 m-10 my-5">
                 <p className="ml-3"> {data.Topic} </p>
                 <img src={Catminkya}  className=" rounded-3xl shadow-2xl" alt="Catminkya" />
              </div>
